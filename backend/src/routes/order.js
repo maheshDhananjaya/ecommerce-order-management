cd35bd15abd26bc74c3dbc8f3bacb73b59629ec0
maheshDhananjaya <dhananjayam94@gmail.com>
2025-11-28 11:05:23 +0530

    feature : define routes

---
+++ backend/src/routes/order.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/order");
+
+router.get("/order", controller.getOrders);
+router.get("/order/:id", controller.getOrderById);
+router.post("/orders", controller.addOrder);
+router.put("/orders/:id", controller.updateOrder);
+router.delete("/orders/:id", controller.deleteOrder);
+
+module.exports = router;
