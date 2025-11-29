62c14d9c143b68f71841c41cefd4ed2e402ef9c9
maheshDhananjaya <dhananjayam94@gmail.com>
2025-11-30 00:17:48 +0530

    fixes : code refactor

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,19 +1,19 @@
-const API_URL = "http://localhost:4000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/";
 
 export const getOrders = () =>
-  fetch(`${API_URL}/order`).then((res) => res.json());
+  fetch(`${API_URL}api/order`).then((res) => res.json());
 
 export const addOrder = (order: any) =>
-  fetch(`${API_URL}/orders`, {
+  fetch(`${API_URL}api/orders`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(order),
   });
 
 export const getProducts = () =>
-  fetch(`${API_URL}/products`).then((res) => res.json());
+  fetch(`${API_URL}api/products`).then((res) => res.json());
 
 export const orderDelete = (id: number) =>
-  fetch(`${API_URL}/orders/${id}`, {
+  fetch(`${API_URL}api/orders/${id}`, {
     method: "DELETE",
   });
